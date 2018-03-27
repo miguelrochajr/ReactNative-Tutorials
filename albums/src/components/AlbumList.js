30471aa5e4fcadc729859eabb022104c548b5a34
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-27 20:10:00 -0300

    Refactoring AlbumList to Class Based Components

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List!</Text>
-    </View>
-  );
-};
+// Class based components
+//  We use class based components when we need a more complex component that
+// more helper methods
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List!</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;
