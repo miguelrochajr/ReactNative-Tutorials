2ab67b70bd8513b25158bdfb18f8890db0d9ebea
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-27 19:16:48 -0300

    First commit. First header compoenent created

---
+++ albums/index.js
@@ -0,0 +1,15 @@
+// Import a library to help create a component
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+// Since we are only returning one thing, ES6 allows you to drop the returning
+// statement and just place the parenthesis. This means "fat arrow returns
+// what's inside of the parenthesis"
+const App = () => (
+  <Header />
+);
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
