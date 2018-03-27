070688cc779e10f33e848261ea72800fb61fa386
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-27 20:02:16 -0300

    AlbumList component created

--- albums/index.js
+++ albums/index.js
@@ -1,14 +1,18 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 // Since we are only returning one thing, ES6 allows you to drop the returning
 // statement and just place the parenthesis. This means "fat arrow returns
 // what's inside of the parenthesis"
 const App = () => (
-  <Header headerText={'Albums'} />
+  <View>
+    <Header headerText={'Albums'} />
+    <AlbumList />
+  </View>
 );
 
 // Render it to the device
