9365ab5efca66e08cc11f46099dac7595b3f72f0
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-27 22:19:49 -0300

    Card components added

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 // Class based components
 //  We use class based components when we need a more complex component that
 // more helper methods
 class AlbumList extends Component {
+  state = { albums: [] };  // class level property
+
+  // Life cycle method executed right before this component is rendered into the screen
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+      // everytime the state is updated, the Component is rendered again
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
   render() {
+    console.log(this.state);
+
     return (
       <View>
-        <Text>Album List!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
