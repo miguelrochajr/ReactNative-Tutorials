2701e3e9486e9cad0623b4a83009bd208b8bf5c9
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-29 12:27:20 -0300

    Common components added

---
+++ auth/src/components/common/Header.js
@@ -0,0 +1,35 @@
+// Improt libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center', // position items on the Y axis
+    alignItems: 'center', // position items on the X axis
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.2,
+    elevation: 2, // explain why later
+    position: 'relative' // explain why later
+  },
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// Make the component available to other parths of the app
+export { Header };
