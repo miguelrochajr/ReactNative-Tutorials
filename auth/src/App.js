a61689a44f972deb258509114207a3e9f93bab24
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-03-29 13:20:44 -0300

    LoginForm added using common components

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAHZz9hsb-lSiePBkyM589Qt755IiEWtvY',
+      authDomain: 'authentication-b5e97.firebaseapp.com',
+      databaseURL: 'https://authentication-b5e97.firebaseio.com',
+      projectId: 'authentication-b5e97',
+      storageBucket: 'authentication-b5e97.appspot.com',
+      messagingSenderId: '53031703058'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>My App!</Text>
+        <LoginForm />
       </View>
     );
   }
