c292466612a7345a6f51daba718d782f8f59e4b0
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-04-01 09:27:33 -0300

    Selection reducer finished

--- tech_stack/src/reducers/SelectionReducer.js
+++ tech_stack/src/reducers/SelectionReducer.js
@@ -1,4 +1,8 @@
-export default (state, action) => {
-    console.log(action);
-    return null;
+export default (state = null, action) => {
+    switch(action.types) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 };
