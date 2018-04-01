c8545e56afb68b4a64a95fb9b615ebf18330ee50
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-04-01 09:18:21 -0300

    Touchable event added to list

--- tech_stack/src/actions/index.js
+++ tech_stack/src/actions/index.js
@@ -1,10 +1,10 @@
 // this function is an action creator
 // an object with a type property is an action. And an action and an action is how we cause
-// producers to update the data
+// producers to update the data through reducers
 //  The action is wrapped by a function, which is the actionCreator
 export const selectLibrary = (libraryId) => {
     return {
         type: 'select_library',
-        payload: 'libraryId'
+        payload: libraryId
     };
 };
--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { connect } from 'react-redux';
 import { CardSection } from './common';
+import * as actions from '../actions';
 
 class ListItem extends Component {
 
     render() {
         const { titleStyle } = styles;
-
+        const { id, title } = this.props.library;
+        
         return(
-            <CardSection>
-                <Text style={titleStyle}>
-                    {this.props.library.title}
-                </Text>
-            </CardSection>
+            <TouchableWithoutFeedback 
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardSection>
+                        <Text style={titleStyle}> 
+                            {title}
+                        </Text>
+                    </CardSection>
+                </View>
+            </TouchableWithoutFeedback>
         );
     }
 }
@@ -24,4 +33,13 @@ const styles = {
     }
 };
 
-export default ListItem;
+/* 
+    - The first argument is the mapStateToProps function. Sice we do not one,
+    we shall pass null.  It is also passed to the Store component
+
+    - The second argument are the actions. Bear in mind that these actions
+    will be binded to the ListItem component as props. Thus, we will be able
+    to access these actions on our componnet through props.
+*/
+// export default ListItem;
+export default connect(null, actions)(ListItem);
