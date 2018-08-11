ad0bb0d5052226e36a5f7b0cc2e34f93c0365ebb
Miguel Rocha Jr <miguelsrochajr@gmail.com>
2018-08-11 05:06:51 -0300

    Header buttons

--- ReactNavigationTest/src/screens/HomeScreen.js
+++ ReactNavigationTest/src/screens/HomeScreen.js
@@ -13,9 +13,31 @@ class LogoTitle extends Component {
 }
 
 class HomeScreen extends Component {
-    static navigationOptions = {
-        // title: 'Home',
-        headerTitle: <LogoTitle />
+    static navigationOptions = ({ navigation }) => {
+        return {
+            // title: 'Home',
+            headerTitle: <LogoTitle />,
+            headerRight: (
+                <Button 
+                    onPress={navigation.getParam('increaseCount')}
+                    title="+1"
+                    color="#fff"
+                />
+            ),
+            headerBackTitle: 'Home'
+        }
+    };
+
+    componentDidMount() {
+        this.props.navigation.setParams({ increaseCount: this._increaseCount });
+      }
+
+    state = {
+        count: 0,
+    };
+
+    _increaseCount = () => {
+        this.setState({ count: this.state.count + 1 });
     };
 
     onDetailsPress() {
@@ -28,7 +50,8 @@ class HomeScreen extends Component {
     render() {
         return (
             <View style={styles.homeStyle}>
-                <Text> Home Screen!</Text>
+                <Text> Home Screen! </Text>
+                <Text> Count: {this.state.count}</Text>
                 <Button 
                     title="Go to Details"
                     onPress={this.onDetailsPress.bind(this)}
